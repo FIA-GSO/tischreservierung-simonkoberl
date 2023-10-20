9b1c31d9ecb14f0f10d2144e6645d7a265629fd3
benzn <ben.montag@web.de>
2023-10-20 09:39:37 +0200

    düdüm

--- api/api.py
+++ api/api.py
@@ -1,12 +1,35 @@
 import flask
-from flask import request   # wird benötigt, um die HTTP-Parameter abzufragen
-from flask import jsonify   # übersetzt python-dicts in json
+from flask import request, jsonify
+import sqlite3
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True  # Zeigt Fehlerinformationen im Browser, statt nur einer generischen Error-Message
+
 
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Tischreservierung</h1>"
 
+
+@app.route("/free_tables", methods=["GET"])
+def get_free_tables():
+
+    connect = sqlite3.connect("buchungssystem.sqlite")
+    query = "SELECT * FROM reservierungen"
+    cur = connect.cursor()
+    res = cur.execute(query)
+    ret = res.fetchall()
+
+    #Request Parameter abfragen
+    if 'zeitpunkt' in request.args:
+        wunschzeitpunkt = str(request.args['zeitpunkt'])
+        for wunschzeitpunkt in ret:
+            pass
+    else:
+        return "Error: Bitte Zeit Angeben"
+
+    return wunschzeitpunkt
+
+
 app.run()
+
